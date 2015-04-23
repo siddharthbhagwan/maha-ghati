c6eefa0a9a47a720b0f4b175175292cac04a73fb
ajay-r-bhat <ajay.r.bhat@gmail.com>
2015-04-23 18:19:51 +0530

    1.Responsive changes. 2.Video links calling changed 3.Bootstrap structure fixed

--- js/main.js
+++ js/main.js
@@ -6,14 +6,35 @@ $(document).ready(function() {
     var vidId = $(this).attr('id');
     last_played = vidId;
     embed_html = '<iframe id="player_'+vidId+'" width="700" height="400" src="http://www.youtube.com/embed/' + vidId + '?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>';
-    console.log(embed_html);
-    $('.player').html(embed_html);
-
+    $('.video-container').html(embed_html);
+    console.log('id ' + vidId);
     new YT.Player('player_'+vidId, {
       events: {
         'onStateChange': onPlayerStateChange
       }
     });
+  });
+
+  $('.next').click(function(){
+    $('#'+last_played).next().click();
+  });
+
+  $('.previous').click(function(){
+    $('#'+last_played).prev().click();
+  });
+
+  $('.player_nav').hover(function(){
+    $(this).css("color","gray");
+  }, function(){
+    $(this).css("color","black");
+  });
+
+  $('.list-element').hover(function(){
+    $(this).css('cursor','pointer');
+    $(this).stop().animate({ fontSize : '15px' });
+  }, function(){
+    $(this).css('cursor','auto');
+    $(this).stop().animate({ fontSize : '14px' });
   });
 
   function onPlayerStateChange(event) {
@@ -22,17 +43,19 @@ $(document).ready(function() {
         $('#'+last_played).next().click();
       break;
       case YT.PlayerState.PLAYING:
-      console.log('Video is playing.');
+      // console.log('Video is playing.');
       break;
       case YT.PlayerState.PAUSED:
-      console.log('Video is paused.');
+      // console.log('Video is paused.');
       break;
       case YT.PlayerState.BUFFERING:
-      console.log('Video is buffering.');
+      // console.log('Video is buffering.');
       break;
       case YT.PlayerState.CUED:
-      console.log('Video is cued.');
+      // console.log('Video is cued.');
       break;
     }
   }
+
+  (function(){$('.list-element').first().click(); console.log('called');}())
 });
