7275baf41f92c0a6ddb610b7828e69ae7dd689b5
Siddharth Bhagwan <sid_sss@hotmail.com>
2015-04-24 21:55:13 +0530

    List updated. Shuffle Enabled

--- js/main.js
+++ js/main.js
@@ -1,36 +1,47 @@
 $(document).ready(function() {
 
-  var last_played = $('.list-element').first().attr('id');
+  var last_played = undefined;
   var total_songs = $('.list-element').length;
   var current_song = $('.list-element').first().attr('id');
 
+
   $('.list-element').click(function(){
-    var vidId = $(this).attr('id');
-    last_played = vidId;
-    var embed_html = '<iframe id="player_'+vidId+'" width="700" height="400" src="http://www.youtube.com/embed/' + vidId + '?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>';
+    last_played = current_song;
+    current_song = $(this).attr('id')
+    var embed_html = '<iframe id="player_' + current_song + '" width="700" height="400" src="http://www.youtube.com/embed/' + current_song + '?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>';
     $('.video-container').html(embed_html);
-    new YT.Player('player_'+vidId, {
+    new YT.Player('player_'+current_song, {
       events: {
         'onStateChange': onPlayerStateChange
       }
     });
   });
 
+
   $('.next').click(function(){
-    $('.shuffle').data('shuffle')? ($('.list-element:eq(' + ((Math.floor(Math.random()*parseInt(total_songs)))-1) + ')').click()):
-      $('#'+last_played).next().length? 
-        $('#'+last_played).next().click() : $('.list-element').first().click(); 
+  ($('.shuffle').attr('data-shuffle') == 'true')?
+      ($('.list-element:eq(' + ((Math.floor(Math.random()*parseInt(total_songs)))-1) + ')').click()):
+        $('#'+current_song).next().length?
+          $('#'+current_song).next().click():
+            $('.list-element').first().click();
   });
+
 
   $('.previous').click(function(){
-    $('#'+last_played).prev().length? $('#'+last_played).prev().click() : $('.list-element').last().click(); 
+    ($('.shuffle').attr('data-shuffle') == 'true')?
+      ($('.list-element:eq(' + ((Math.floor(Math.random()*parseInt(total_songs)))-1) + ')').click()):
+        $('#'+current_song).prev().length? 
+          $('#'+current_song).prev().click():
+            $('.list-element').last().click(); 
   });
 
-  // $('.player_nav').hover(function(){
-  //   $(this).css('color','gray');
-  // }, function(){
-  //   $(this).css('color','black');
-  // });
+
+  $('.player_nav').hover(function(){
+    $(this).css('color','gray');
+  }, function(){
+    $(this).css('color','black');
+  });
+
 
   $('.list-element').hover(function(){
     $(this).css('cursor','pointer');
@@ -40,21 +51,38 @@ $(document).ready(function() {
     $(this).stop().animate({ fontSize : '14px' });
   });
 
-  $('.shuffle').click(function(){
-    if($(this).attr('data-shuffle') == 'false'){
-      console.log('cahnge to true');
-      $(this).attr('data-shuffle','true');
-      $(this).css('color','red');
-    } else {
-      $(this).attr('data-shuffle','false');
-      $(this).css('color','black');
+
+  $('.shuffle').click(function(e, from){
+    if(from == null){
+      if($(this).attr('data-shuffle') == 'false'){
+        $(this).addClass('ripple');
+        $(this).attr('data-shuffle','true');
+        $(this).css('color','white');
+        $(this).addClass('shuffle_on');
+        $(this).addClass('fa-4x');
+        shuffle_start = setInterval(function () {
+          $('.shuffle').trigger('click', ['Trigger'])
+          }, 700);
+      } else {
+        $(this).removeClass('ripple');
+        $(this).attr('data-shuffle','false');
+        $(this).css('color','black');
+        $(this).removeClass('shuffle_on');
+        $(this).removeClass('fa-4x');
+        clearInterval(shuffle_start);
+      } 
     }
   });
+
 
   function onPlayerStateChange(event) {
     switch(event.data) {
       case YT.PlayerState.ENDED:
-        $('#'+last_played).next().click();
+        $('.shuffle').data('shuffle')?
+          ($('.list-element:eq(' + ((Math.floor(Math.random()*parseInt(total_songs)))-1) + ')').click()):
+            $('#'+last_played).next().length? 
+              $('#'+last_played).next().click():
+                $('.list-element').first().click();
       break;
       case YT.PlayerState.PLAYING:
       // console.log('Video is playing.');
@@ -70,6 +98,51 @@ $(document).ready(function() {
       break;
     }
   }
+});
 
-  (function(){$('.list-element').first().click();}())
+var start = function(){
+  $('.list-element').first().click();
+};
+
+// Ripple
+
+(function (window, $) {
+  
+  $(function() {
+    
+    
+    $('.ripple').on('click', function (event) {
+      event.preventDefault();
+      
+      var $div = $('<div/>'),
+          btnOffset = $(this).offset(),
+          xPos = event.pageX - btnOffset.left,
+          yPos = event.pageY - btnOffset.top;
+      
+
+      
+      $div.addClass('ripple-effect');
+      var $ripple = $(".ripple-effect");
+      
+      $ripple.css("height", $(this).height());
+      $ripple.css("width", $(this).height());
+      $div
+        .css({
+          top: yPos - ($ripple.height()/2),
+          left: xPos - ($ripple.width()/2),
+          background: $(this).data("ripple-color")
+        }) 
+        .appendTo($(this));
+
+      window.setTimeout(function(){
+        $div.remove();
+      }, 2000);
+    });
+    
+  });
+  
+})(window, jQuery);
+
+$(document).ready(function() {
+  start();
 });
