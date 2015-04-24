263047748bcad29e92d4b08c6f790a58983d3ed0
Siddharth Bhagwan <sid_sss@hotmail.com>
2015-04-24 17:16:54 +0530

    GA added. Player fns fixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
-  var last_played;
+  var last_played = $('.list-element').first().attr('id');
+  var total_songs = $('.list-element').length;
+  var current_song = $('.list-element').first().attr('id');
 
   $('.list-element').click(function(){
     var vidId = $(this).attr('id');
     last_played = vidId;
-    embed_html = '<iframe id="player_'+vidId+'" width="700" height="400" src="http://www.youtube.com/embed/' + vidId + '?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>';
+    var embed_html = '<iframe id="player_'+vidId+'" width="700" height="400" src="http://www.youtube.com/embed/' + vidId + '?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>';
     $('.video-container').html(embed_html);
     new YT.Player('player_'+vidId, {
       events: {
@@ -15,19 +17,20 @@ $(document).ready(function() {
   });
 
   $('.next').click(function(){
-    $('#'+last_played).next().click();
+    $('.shuffle').data('shuffle')? ($('.list-element:eq(' + ((Math.floor(Math.random()*parseInt(total_songs)))-1) + ')').click()):
+      $('#'+last_played).next().length? 
+        $('#'+last_played).next().click() : $('.list-element').first().click(); 
   });
 
   $('.previous').click(function(){
-    console.log($('#'+last_played).prev());
-    $('#'+last_played).prev().click();
+    $('#'+last_played).prev().length? $('#'+last_played).prev().click() : $('.list-element').last().click(); 
   });
 
-  $('.player_nav').hover(function(){
-    $(this).css("color","gray");
-  }, function(){
-    $(this).css("color","black");
-  });
+  // $('.player_nav').hover(function(){
+  //   $(this).css('color','gray');
+  // }, function(){
+  //   $(this).css('color','black');
+  // });
 
   $('.list-element').hover(function(){
     $(this).css('cursor','pointer');
@@ -35,6 +38,17 @@ $(document).ready(function() {
   }, function(){
     $(this).css('cursor','auto');
     $(this).stop().animate({ fontSize : '14px' });
+  });
+
+  $('.shuffle').click(function(){
+    if($(this).attr('data-shuffle') == 'false'){
+      console.log('cahnge to true');
+      $(this).attr('data-shuffle','true');
+      $(this).css('color','red');
+    } else {
+      $(this).attr('data-shuffle','false');
+      $(this).css('color','black');
+    }
   });
 
   function onPlayerStateChange(event) {
