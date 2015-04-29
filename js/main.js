7ac194db9826328cef33b3b2be7a357f9e1f7a06
Siddharth Bhagwan <sid_sss@hotmail.com>
2015-04-29 21:07:31 +0530

    Ripple Removed. JS Refactored. Songs re-added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,6 @@
-
-var last_played = undefined;
-var total_songs = $('.list-element').length;
-var current_song = $('.list-element').first().attr('id');
+var last_played, total_songs, current_song;
 
 function clickSong(){
-
-  // function onPlayerStateChange(event) {
-  //   switch(event.data) {
-  //     case YT.PlayerState.ENDED:
-  //       $('.shuffle').data('shuffle')?
-  //         ($('.list-element:eq(' + ((Math.floor(Math.random()*parseInt(total_songs)))-1) + ')').click()):
-  //           $('#'+last_played).next().length? 
-  //             $('#'+last_played).next().click():
-  //               $('.list-element').first().click();
-  //     break;
-  //     case YT.PlayerState.PLAYING:
-  //     // console.log('Video is playing.');
-  //     break;
-  //     case YT.PlayerState.PAUSED:
-  //     // console.log('Video is paused.');
-  //     break;
-  //     case YT.PlayerState.BUFFERING:
-  //     // console.log('Video is buffering.');
-  //     break;
-  //     case YT.PlayerState.CUED:
-  //     // console.log('Video is cued.');
-  //     break;
-  //   }
-  // }
-
-
   var embed_html;
   last_played = current_song;
   current_song = $(this).attr('id')
@@ -37,11 +8,16 @@ function clickSong(){
   $('.video-container').html(embed_html);
   new YT.Player('player_'+current_song, {
     events: {
-      'onStateChange': function(){
-        console.log('yay');
-      }
+      'onStateChange': onPlayerStateChange
     }
   });
+}
+
+function onPlayerStateChange(event) {
+  switch(event.data) {
+    case YT.PlayerState.ENDED: $('.next').click();
+    break;
+  }
 }
 
 function highlightPlayerNavs(){
@@ -53,7 +29,7 @@ function downplayPlayerNavs(){
 }
 
 function nextSong(){
-($('.shuffle').attr('data-shuffle') == 'true')?
+  ($('.shuffle').attr('data-shuffle') == 'true')?
     ($('.list-element:eq(' + ((Math.floor(Math.random()*parseInt(total_songs)))-1) + ')').click()):
       $('#'+current_song).next().length?
         $('#'+current_song).next().click():
@@ -78,81 +54,64 @@ function downplayList(){
   $(this).stop().animate({ fontSize : '14px' });
 }
 
+function toggleShuffle(event, from){
+  if(from == null){
+    if($(this).attr('data-shuffle') == 'false'){
+      $(this).addClass('shuffle_on fa-4x');
+      $(this).attr('data-shuffle','true');
+      $(this).css('color','white');
+      // $(this).addClass('ripple');
+      // shuffle_start = setInterval(function () {
+      //   $('.shuffle').trigger('click', ['Trigger'])
+      //   }, 700);
+    } else {
+      $(this).removeClass('shuffle_on fa-4x');
+      $(this).attr('data-shuffle','false');
+      $(this).css('color','black');
+      // $(this).removeClass('ripple');
+      clearInterval(shuffle_start);
+    } 
+  }
+}
+
+// Ripple
+// function createRipple(event) {
+//   event.preventDefault();
+
+//   var $div = $('<div/>'),
+//       btnOffset = $(this).offset(),
+//       xPos = event.pageX - btnOffset.left,
+//       yPos = event.pageY - btnOffset.top;
+  
+//   $div.addClass('ripple-effect');
+//   var $ripple = $(".ripple-effect");
+  
+//   $ripple.css("height", $(this).height());
+//   $ripple.css("width", $(this).height());
+//   $div
+//     .css({
+//       top: yPos - ($ripple.height()/2),
+//       left: xPos - ($ripple.width()/2),
+//       background: $(this).data("ripple-color")
+//     }) 
+//     .appendTo($(this));
+
+//   window.setTimeout(function(){
+//     $div.remove();
+//   }, 2000);
+// }
+
 $(document).ready(function() {
+
+  last_played = undefined;
+  total_songs = $('.list-element').length;
+  current_song = $('.list-element').first().attr('id');
+
   $('.list-element').click(clickSong);
   $('.player_nav').hover(highlightPlayerNavs, downplayPlayerNavs);
   $('.next').click(nextSong);
   $('.previous').click(previousSong);
-  $('.list-element').hover(highlightList, downplayList)
+  $('.list-element').hover(highlightList, downplayList);
+  $('.shuffle').click(toggleShuffle);
+  $('.list-element').first().click();
 });
-
-
-  // $('.shuffle').click(function(e, from){
-  //   if(from == null){
-  //     if($(this).attr('data-shuffle') == 'false'){
-  //       $(this).addClass('ripple');
-  //       $(this).attr('data-shuffle','true');
-  //       $(this).css('color','white');
-  //       $(this).addClass('shuffle_on');
-  //       $(this).addClass('fa-4x');
-  //       shuffle_start = setInterval(function () {
-  //         $('.shuffle').trigger('click', ['Trigger'])
-  //         }, 700);
-  //     } else {
-  //       $(this).removeClass('ripple');
-  //       $(this).attr('data-shuffle','false');
-  //       $(this).css('color','black');
-  //       $(this).removeClass('shuffle_on');
-  //       $(this).removeClass('fa-4x');
-  //       clearInterval(shuffle_start);
-  //     } 
-  //   }
-  // });
-// });
-
-// var start = function(){
-//   $('.list-element').first().click();
-// };
-
-// Ripple
-
-// (function (window, $) {
-  
-//   $(function() {
-    
-    
-//     $('.ripple').on('click', function (event) {
-//       event.preventDefault();
-      
-//       var $div = $('<div/>'),
-//           btnOffset = $(this).offset(),
-//           xPos = event.pageX - btnOffset.left,
-//           yPos = event.pageY - btnOffset.top;
-      
-
-      
-//       $div.addClass('ripple-effect');
-//       var $ripple = $(".ripple-effect");
-      
-//       $ripple.css("height", $(this).height());
-//       $ripple.css("width", $(this).height());
-//       $div
-//         .css({
-//           top: yPos - ($ripple.height()/2),
-//           left: xPos - ($ripple.width()/2),
-//           background: $(this).data("ripple-color")
-//         }) 
-//         .appendTo($(this));
-
-//       window.setTimeout(function(){
-//         $div.remove();
-//       }, 2000);
-//     });
-    
-//   });
-  
-// })(window, jQuery);
-
-// $(document).ready(function() {
-//   start();
-// });
